f87ba117d2f03234600cc12f7721ed07f0651a36
PaulQuispe3000 <arturo.quispe3000@gamil.com>
2023-07-14 11:27:35 +0200

    refactor structure

---
+++ index.js
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded', function() {
+    window.location.href = 'http://127.0.0.1:5500/index.html#img0';
+});
--- page/index.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    window.location.href = 'https://127.0.0.1:5500/index.html#img0';
-});
